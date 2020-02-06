c85c2fb90daa51495a064ccdd62e54c330b6ac19
po_miyasaka <miyasaka@yahoo.co.jp>
2020-02-07 02:46:49 +0900

    test for is_favorite

--- spec/dmm_spec.rb
+++ spec/dmm_spec.rb
@@ -5,9 +5,18 @@ require "./dmm_lib.rb"
 
 RSpec.describe "dmm_lib" do
   describe "#generate_url_string" do
-    it "works" do
-      expect(generate_url_string("2020-02-03", "22:00"))
-        .to eq("https://eikaiwa.dmm.com/list/?&data[tab1][start_time]=22:00&data[tab1][end_time]=22:00&data[tab1][native]=1&data[tab1][favorite]=on&date=2020-02-03&sort=6")
+    context "favorite is true" do
+      it "works" do
+        expected = "https://eikaiwa.dmm.com/list/?&data[tab1][start_time]=22:00&data[tab1][end_time]=22:00&data[tab1][native]=1&data[tab1][favorite]=on&date=2020-02-03&sort=6"
+        expect(generate_url_string("2020-02-03", "22:00", true)).to eq(expected)
+      end
+    end
+
+    context "favorite is false" do
+      it "works" do
+        expected = "https://eikaiwa.dmm.com/list/?&data[tab1][start_time]=22:30&data[tab1][end_time]=22:30&data[tab1][native]=1&date=2021-02-30&sort=6"
+        expect(generate_url_string("2021-02-30", "22:30", false)).to eq(expected)
+      end
     end
   end
 end
