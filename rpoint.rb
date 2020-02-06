49c3b71a32be7f5b7a27a0837b765af4af37c53f
po_miyasaka <miyasaka@yahoo.co.jp>
2020-02-07 06:09:25 +0900

    fix label

--- rpoint.rb
+++ rpoint.rb
@@ -3,7 +3,7 @@
 require "webdrivers"
 require "uri"
 
-LOGINID     =  ARGV[0] || ENV["$RLOGINID"]
+LOGINID = ARGV[0] || ENV["$RLOGINID"]
 PASSWORD    = ARGV[1] || ENV["$RPASSWORD"]
 LOGINURL    = ARGV[2] || ENV["$RLOGINURL"]
 CAMPAIGNURL = ARGV[3] || ENV["$RCAMPAIGNURL"]
@@ -19,19 +19,17 @@ statuses = driver.find_elements(:class, "campaign__status--saLnK")
 all_links = driver.find_elements(:class, "campaign__title--1qpNU")
 
 campaign_links = statuses.zip(all_links)
-.filter { |e| e[0].text == "エントリー受付中" }
-.map { |e| e[1].find_element(:xpath, ".//a").attribute("href") }
+                         .filter { |e| e[0].text == "エントリー受付中" }
+                         .map { |e| e[1].find_element(:xpath, ".//a").attribute("href") }
 
-puts "キャンペーン一覧"
+puts "Canpaign List"
 puts campaign_links
 failed = campaign_links.map do |canpaign_link|
   driver.navigate.to(canpaign_link)
   links = driver.find_elements(:xpath, "//a[starts-with(@href,'https://oubo')]")
-  if (links.empty?)
-    return campaign_link
-  end
-  
-  link = links[0].attribute("href") 
+  return campaign_link if links.empty?
+
+  link = links[0].attribute("href")
   puts link
   driver.navigate.to(link)
   sleep(1)
