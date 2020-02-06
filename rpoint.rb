78a1d5058c5a1f95664c517af88d33d7e3af9ff4
po_miyasaka <miyasaka@yahoo.co.jp>
2020-02-07 06:07:10 +0900

    active code

--- rpoint.rb
+++ rpoint.rb
@@ -3,53 +3,42 @@
 require "webdrivers"
 require "uri"
 
-LOGINID =  ARGV[0] || ENV["$RAKUTENLOGINID"]
-PASSWORD = ARGV[1] || ENV["$RAKUTENPASSWORD"]
-ENV["TZ"] = "Asia/Tokyo"
+LOGINID     =  ARGV[0] || ENV["$RLOGINID"]
+PASSWORD    = ARGV[1] || ENV["$RPASSWORD"]
+LOGINURL    = ARGV[2] || ENV["$RLOGINURL"]
+CAMPAIGNURL = ARGV[3] || ENV["$RCAMPAIGNURL"]
 
 driver = Selenium::WebDriver.for :chrome
-driver.navigate.to "https://grp03.id.rakuten.co.jp/rms/nid/login?service_id=i122"
+driver.navigate.to(LOGINURL)
 
 driver.find_element(:id, "loginInner_u").send_keys(LOGINID)
 driver.find_element(:id, "loginInner_p").send_keys(PASSWORD)
 driver.find_element(:class, "loginButton").click
+driver.navigate.to(CAMPAIGNURL)
+statuses = driver.find_elements(:class, "campaign__status--saLnK")
+all_links = driver.find_elements(:class, "campaign__title--1qpNU")
 
-def doSomething(driver)
-  driver.navigate.to "https://pointcard.rakuten.co.jp/campaign/"
-  statuss = driver.find_elements(:class, "campaign__status--saLnK")
-  links = driver.find_elements(:class, "campaign__title--1qpNU")
+campaign_links = statuses.zip(all_links)
+.filter { |e| e[0].text == "エントリー受付中" }
+.map { |e| e[1].find_element(:xpath, ".//a").attribute("href") }
 
-  tuples = statuss.zip(links)
-
-  tuples.each do |canpaign|
-    puts "yyyyyyyyyyyyyyyy"
-    if canpaign[0].text == "エントリー受付中"
-      canpaign[1].click
-      button_elements = driver.find_elements(:class, "entry-Btn")
-      puts("aa")
-      puts button_elements.count
-      if button_elements.count == 0
-        button_elements = driver.find_elements(:class, "entryBtn")
-      end
-      puts button_elements.count
-      button_elements.each do |button_element|
-        puts "ccccccccccccc"
-        link = button_element.attribute("href")
-        puts "ddddddddddd"
-        unless link.nil?
-          driver.navigate.to link
-          throw :next
-        end
-      end
-    end
-    throw :next
+puts "キャンペーン一覧"
+puts campaign_links
+failed = campaign_links.map do |canpaign_link|
+  driver.navigate.to(canpaign_link)
+  links = driver.find_elements(:xpath, "//a[starts-with(@href,'https://oubo')]")
+  if (links.empty?)
+    return campaign_link
   end
+  
+  link = links[0].attribute("href") 
+  puts link
+  driver.navigate.to(link)
+  sleep(1)
+  driver.navigate.to(CAMPAIGNURL)
+  return nil
 end
 
-20.times do
-  catch (:next) do
-    puts "bbbb"
-    doSomething(driver)
-  end
+failed.compact.each do |link|
+  puts link
 end
-puts "aaaa"
